4dbeb11905aff3863535b683184e71dcf37006c0
shallow-and-limited <courchesne.alex@gmail.com>
2026-02-25 13:31:36 -0500

    :zap: Fake age gate (DNU)

--- storefront/assets/global.js
+++ storefront/assets/global.js
@@ -1894,3 +1894,45 @@ class QuickviewContainer extends HTMLElement {
 }
 
 customElements.define('quickview-container', QuickviewContainer);
+
+
+
+class AgeGate extends HTMLElement {
+  constructor() {
+    super();
+
+    this.body = document.body;
+    this.form = this.querySelector('form');
+    this.content = this.querySelector('.age-gate__content');
+    this.denied = this.querySelector('.age-gate__denied');
+
+    this.allow = localStorage.getItem('age-gate-allow');
+
+    if(this.allow === 'true') {
+      this.remove();
+      this.body.classList.remove('verification-required');
+    }
+
+    this.form.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this.form);
+    const agePermitted = formData.get('age-permitted');
+
+    if(agePermitted === 'yes') {
+      localStorage.setItem('age-gate-allow', 'true');
+      this.remove();
+      this.body.classList.remove('verification-required');
+    } else {
+      localStorage.setItem('age-gate-allow', 'false');
+     
+      this.content.classList.add('hidden');
+      this.denied.classList.remove('hidden');
+    }
+  }
+}
+
+customElements.define('age-gate', AgeGate);
